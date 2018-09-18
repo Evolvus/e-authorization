acf408011f5b40b61e57c2c1e490b53fa5c4afbf
KmKavya <kavyavshaiva@gmail.com>
2018-09-18 11:58:52 +0530

    added authorize api

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,62 +1,32 @@
-const debug = require("debug")("evolvus-platform-server:routes:api:user");
+const debug = require("debug")("e-authorization:routes:api:user");
 const _ = require("lodash");
-const user = require("evolvus-user");
-const application = require("evolvus-application");
+const user = require("@evolvus/evolvus-user");
 
-const headerAttributes = ["tenantid", "entityid", "accesslevel"];
-const authCredentials = ["userName", "applicationCode"];
+const authCredentials = ["userId", "corporateId", "roleId"];
 
 module.exports = (router) => {
 
-    router.route("/user/authorize")
-        .post((req, res, next) => {
-            try {
-                let body = _.pick(req.body, authCredentials);
-                user.authorize(body).then((user) => {
-                    res.status(200).send(user);
-                }).catch((e) => {
-                    const error = {
-                        status: '404',
-                        data: e,
-                        description: `${e.toString()}`
-                    }
-                    res.status(404).send(error);
-                });
-            } catch (e) {
-                const error = {
-                    status: '404',
-                    data: e,
-                    description: `${e.toString()}`
-                }
-                res.status(404).send(error);
-            }
+  router.route("/user/authorize")
+    .post((req, res, next) => {
+      try {
+        let body = _.pick(req.body, authCredentials);
+        user.authorize(body).then((user) => {
+          res.status(200).send(user);
+        }).catch((e) => {
+          const error = {
+            status: '404',
+            data: e,
+            description: `${e.toString()}`
+          }
+          res.status(404).send(error);
         });
-
-    router.route("/user/updateToken")
-        .post((req, res, next) => {
-            try {
-                let body = _.pick(req.body, ["token", "id"]);
-                console.log('Updating toke for the user', body.id);
-                user.updateToken(body.id, body.token).then((user) => {
-                    res.status(200).send(user);
-
-                }).catch((e) => {
-                    const error = {
-                        status: '404',
-                        data: e,
-                        description: `${e.toString()}`
-                    }
-                    res.status(404).send(error);
-
-                });
-            } catch (e) {
-                const error = {
-                    status: '404',
-                    data: e,
-                    description: `${e.toString()}`
-                }
-                res.status(404).send(error);
-
-            }
-        });
+      } catch (e) {
+        const error = {
+          status: '404',
+          data: e,
+          description: `${e.toString()}`
+        }
+        res.status(404).send(error);
+      }
+    });
 };
