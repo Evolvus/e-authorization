dd844740323c8dfe37b597ef4973427d8e14113a
KmKavya <kavyavshaiva@gmail.com>
2018-09-18 12:33:03 +0530

    added response object

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,30 +3,34 @@ const _ = require("lodash");
 const user = require("@evolvus/evolvus-user");
 
 const authCredentials = ["userId", "corporateId", "roleId"];
+const response = {
+  "status": "200",
+  "description": "",
+  "data": {}
+};
 
 module.exports = (router) => {
 
   router.route("/user/authorize")
     .post((req, res, next) => {
       try {
+        
         let body = _.pick(req.body, authCredentials);
         user.authorize(body).then((user) => {
-          res.status(200).send(user);
+          response.data=user;
+          response.description="User found";
+          res.status(200).send(response);
         }).catch((e) => {
-          const error = {
-            status: '404',
-            data: e,
-            description: `${e.toString()}`
-          }
-          res.status(404).send(error);
+          response.status="404"; 
+          response.data= e;
+          response.description= `${e.toString()}`;
+          res.status(404).send(response);
         });
       } catch (e) {
-        const error = {
-          status: '404',
-          data: e,
-          description: `${e.toString()}`
-        }
-        res.status(404).send(error);
+        response.status="404"; 
+          response.data= e;
+          response.description= `${e.toString()}`;
+          res.status(404).send(response);
       }
     });
 };
