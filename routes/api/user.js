409679670e8f1aaf810652d3faea8f802e86b1ff
Bandaru Mahendra Rao <EVOLVUS\mahendrar@ELPBLR-077.evolvus.com>
2018-10-31 12:25:48 +0530

    Modified for sso login

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,7 +7,7 @@ var corporateURL = process.env.CORPORATE_URL || "http://localhost:8282/flux-serv
 var mainBranch = process.env.ENTITY_ID || "H001B001";
 var timeOut = process.env.TIME_OUT || 5000;
 
-const authCredentials = ["userId", "corporateId", "roleId"];
+const authCredentials = ["userId"];
 
 var instance = axios.create({
   baseURL: corporateURL,
@@ -25,32 +25,16 @@ module.exports = (router) => {
       };
       try {
         let body = _.pick(req.body, authCredentials);
-        instance.get(body.corporateId).then((result) => {
-          if (result.data != null && result.data.data != null) {
-            if (result.data.data.status != null && result.data.data.status.toUpperCase() == "ACTIVE") {
-              user.authorize(body).then((userObj) => {
-                response.data = userObj;
-                response.description = "User found";
-                debug(`user found: ${userObj.userId}`);
-                res.status(200).send(userObj);
-              }).catch((e) => {
-                debug(`Authorization Failed due to ${e} `);
-                response.description = `Authorization Failed due to ${e.toString()}`;
-                res.status(404).send(response);
-              });
-            } else {
-              response.description = "Corporate not found or it is Inactive";
-              debug("Corporate not found or it is Inactive");
-              res.status(404).send(response);
-            }
-          } else {
-            debug("Server Error.Please contact Administrator");
-            res.status(404).send(response);
-          }
+        user.authorize(body).then((userObj) => {         
+          response.data = userObj;
+          response.description = "User found";
+          debug(`user found: ${userObj.userId}`);
+          res.status(200).send(userObj);
         }).catch((e) => {
-          debug("Server Error.Please contact Administrator");
+          debug(`Authorization Failed due to ${e} `);
+          response.description = `Authorization Failed due to ${e.toString()}`;
           res.status(404).send(response);
-        });
+        });       
       } catch (e) {
         debug(`Try-catch failed due to ${e}`);
         response.data = e;
