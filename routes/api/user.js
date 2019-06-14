b65859797995cbc7285309a8900c6b34d24b505a
Kavya K M <EVOLVUS\kavyak@ELPBLR-042.evolvus.com>
2019-06-14 15:33:32 +0530

    restricted user login through SSO if the corporate is INACTIVE

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,16 +3,12 @@ const _ = require("lodash");
 const user = require("@evolvus/evolvus-user");
 const axios = require("axios");
 
-var corporateURL = process.env.CORPORATE_URL || "http://localhost:8282/flux-services/corporate/status/";
+var corporateURL = process.env.CORPORATE_URL || "http://192.168.1.151:8286/flux-services/corporate/status/";
 var mainBranch = process.env.ENTITY_ID || "H001B001";
 var timeOut = process.env.TIME_OUT || 5000;
 
 const authCredentials = ["userId"];
 
-var instance = axios.create({
-  baseURL: corporateURL,
-  timeout: timeOut
-});
 
 module.exports = (router) => {
 
@@ -25,16 +21,43 @@ module.exports = (router) => {
       };
       try {
         let body = _.pick(req.body, authCredentials);
-        user.authorize(body).then((userObj) => {         
-          response.data = userObj;
-          response.description = "User found";
-          debug(`user found: ${userObj.userId}`);
-          res.status(200).send(userObj);
-        }).catch((e) => {
-          debug(`Authorization Failed due to ${e} `);
-          response.description = `Authorization Failed due to ${e.toString()}`;
-          res.status(404).send(response);
-        });       
+        debug("Input corporateId is", req.body.corporateid);
+        axios.get(`${corporateURL}${req.body.corporateid}`).then((resp) => {
+          debug(`Response from ${corporateURL}${req.body.corporateid} is ${JSON.stringify(resp.data)}`);
+          if (resp.data != null && resp.data.data != null && resp.data.data.exist == true) {
+            if (resp.data.data.status != null && resp.data.data.status.toUpperCase() == "ACTIVE") {
+              user.authorize(body).then((userObj) => {
+                response.data = userObj;
+                response.description = "User found";
+                debug(`user found: ${userObj.userId}`);
+                res.status(200).send(userObj);
+              }).catch((e) => {
+                debug(`Authorization Failed due to ${e} `);
+                response.description = `Authorization Failed due to ${e.toString()}`;
+                res.status(404).send(response);
+              });
+            } else {
+              response.status = "400";
+              response.description = "Corporate is INACTIVE";
+              response.data = {};
+              debug("response: " + JSON.stringify(response));
+              res.status(400).send(response);
+            }
+          } else {
+            response.status = "400";
+            response.description = "Corporate not found";
+            response.data = {};
+            debug("response: " + JSON.stringify(response));
+            res.status(400).send(response);
+          }
+        }).catch((error) => {
+          debug(`failed to create user due to`, error);
+          response.status = "400";
+          response.description = "Server Error.Please contact Administrator";
+          response.data = {};
+          debug("response: " + JSON.stringify(response));
+          res.status(400).json(response);
+        });
       } catch (e) {
         debug(`Try-catch failed due to ${e}`);
         response.data = e;
